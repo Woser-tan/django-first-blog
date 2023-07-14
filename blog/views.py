f8e22c88e91ec5818afe70a976febbb5bdca28c6
Tevfik Tan Onay <tevfiktanonay@gmail.com>
2023-07-14 13:09:36 +0300

    Tried filtering with models.Manager

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,17 @@ from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.views import View
+"""
+class PostView(View):
+
+    view_select = None
+
+    def get(self, request, view_select):
+        if(view_select == 'post_list'):
+            posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+            return render(request, 'blog/post_list.html', {'posts': posts})
+"""
+
 
 class PostList(View):
 
@@ -12,73 +23,69 @@ class PostList(View):
         posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
         return render(request, 'blog/post_list.html', {'posts': posts})
 
-
+    
 class PostDetail(View):
-    def get(self,request, pk):
+
+    def get(self, request, pk):
         post = get_object_or_404(Post, pk=pk)
         return render(request, 'blog/post_detail.html', {'post': post})
-
-
+    
 class PostNew(View):
+
     def post(self, request):
         form = PostForm(request.POST)
-
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            return redirect('post_detail', pk=post.pk)
-            
+            return redirect('post_detail', pk=post.pk) 
+        
     def get(self, request):
         form = PostForm()
         return render(request, 'blog/post_edit.html', {'form': form})
 
-
 class PostEdit(View):
+
     def get(self, request, pk):
         post = get_object_or_404(Post, pk=pk)
         form = PostForm(instance=post)
         return render(request, 'blog/post_edit.html', {'form': form})
     
+    
     def post(self, request, pk):
         post = get_object_or_404(Post, pk=pk)
         form = PostForm(request.POST, instance=post)
-
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.save()
             return redirect('post_detail', pk=post.pk)
-
-
+        
 class PostDraftList(View):
-    
     def get(self, request):
         posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
         return render(request, 'blog/post_draft_list.html', {'posts': posts})
-
-
+    
 class PostPublish(View):
-
     def get(self, request, pk):
         post = get_object_or_404(Post, pk=pk)
         post.publish()
         return redirect('post_detail', pk=pk)
     
-
+    
 class PostRemove(View):
     def get(self, request, pk):
         post=get_object_or_404(Post, pk=pk)
         post.delete()
         return redirect('post_list')
+    
 
 class AddCommentToPost(View):
-    
     def get(self, request, pk):
         post = get_object_or_404(Post, pk=pk)
         form = CommentForm()
         return render(request, 'blog/add_comment_to_post.html', {'form': form})
-
+    
     def post(self, request, pk):
         post = get_object_or_404(Post, pk=pk)
         form = CommentForm(request.POST)
@@ -88,20 +95,18 @@ class AddCommentToPost(View):
             comment.save()
             return redirect('post_detail', pk=post.pk)
 
-
 class CommentApprove(View):
     def get(self, request, pk):
         comment = get_object_or_404(Comment, pk=pk)
         comment.approve()
         return redirect('post_detail', pk=comment.post.pk)
-
-
+    
 class CommentRemove(View):
     def get(self, request, pk):
         comment = get_object_or_404(Comment, pk=pk)
         comment.delete()
         return redirect('post_detail', pk=comment.post.pk)
-
+    
 
 # def post_list(request):
 #     posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
