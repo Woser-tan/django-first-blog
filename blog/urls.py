37f978a3bbe0379592ac36353885660f6f32d6fd
Tevfik Tan Onay <tevfiktanonay@gmail.com>
2023-07-21 17:49:40 +0300

    refactor: use ViewSets

--- blog/urls.py
+++ blog/urls.py
@@ -1,16 +1,52 @@
+from blog.views import PostViewSet, CommentViewSet
+from rest_framework import renderers
+from rest_framework.urlpatterns import format_suffix_patterns
 from django.urls import path
-from . import views
 from django.contrib.auth.decorators import login_required
 
-urlpatterns = [
-    path('', views.PostList.as_view(), name = 'post_list'),
-    path('post/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
-    path('post/new/', login_required(views.PostNew.as_view()), name='post_new'),
-    path('post/<int:pk>/edit/', login_required(views.PostEdit.as_view()), name='post_edit'),
-    path('drafts/', login_required(views.PostDraftList.as_view()), name='post_draft_list'),
-    path('post/<pk>/publish/', login_required(views.PostPublish.as_view()), name='post_publish'),
-    path('post/<pk>/remove/', login_required(views.PostRemove.as_view()), name='post_remove'),
-    path('post/<int:pk>/comment/', login_required(views.AddCommentToPost.as_view()), name='add_comment_to_post'),
-    path('comment/<int:pk>/approve/', login_required(views.CommentApprove.as_view()), name='comment_approve'),
-    path('comment/<int:pk>/remove/', login_required(views.CommentRemove.as_view()), name='comment_remove'),
-]
+post_list = PostViewSet.as_view({
+    'get' : 'list'
+})
+post_detail = login_required(PostViewSet.as_view({
+    'get' : 'retrieve'
+}))
+post_new = login_required(PostViewSet.as_view({
+    'get' : 'create',
+    'post' : 'create'
+}))
+post_edit = login_required(PostViewSet.as_view({
+    'get': 'update',
+    'post': 'update'
+}))
+post_draft_list = login_required(PostViewSet.as_view({
+    'get': 'list'
+}))
+post_publish = login_required(PostViewSet.as_view({
+    'get': 'update'
+}))
+post_remove = login_required(PostViewSet.as_view({
+    'get': 'destroy'
+}))
+add_comment_to_post = login_required(CommentViewSet.as_view({
+    'get': 'create',
+    'post': 'create'
+}))
+comment_approve = login_required(CommentViewSet.as_view({
+    'get': 'update'
+}))
+comment_remove = login_required(CommentViewSet.as_view({
+    'get': 'destroy'
+}))
+
+urlpatterns = format_suffix_patterns([
+    path('', post_list, name = 'post_list'),
+    path('post/<int:pk>/', post_detail, name='post_detail'),
+    path('post/new/', login_required(post_new), name='post_new'),
+    path('post/<int:pk>/edit/', login_required(post_edit), name='post_edit'),
+    path('drafts/', login_required(post_draft_list), name='post_draft_list'),
+    path('post/<pk>/publish/', login_required(post_publish), name='post_publish'),
+    path('post/<pk>/remove/', login_required(post_remove), name='post_remove'),
+    path('post/<int:pk>/comment/', login_required(add_comment_to_post), name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', login_required(comment_approve), name='comment_approve'),
+    path('comment/<int:pk>/remove/', login_required(comment_remove), name='comment_remove'),
+])
--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,91 @@ from blog.forms import PostForm, CommentForm
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import TemplateHTMLRenderer
-from rest_framework import status
+from rest_framework import viewsets
 from django.shortcuts import redirect, render, get_object_or_404
 from django.utils import timezone
+from django.urls import resolve
 
+class PostViewSet(viewsets.ModelViewSet):
 
+    renderer_classes = [TemplateHTMLRenderer]
+    
+    def list(self, request):
+        if resolve(request.path_info).url_name == 'post_draft_list':
+            posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+            return Response({'posts': posts}, template_name='blog/post_draft_list.html')
+        else:
+            posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+        return Response({'posts': posts}, template_name='blog/post_list.html')
+    
+    def retrieve(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        return Response({'post': post}, template_name='blog/post_detail.html')
+        
+    def create(self, request):
+        if request.method == 'POST':
+            form = PostForm(request.POST)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            form = PostForm()
+        return Response({'form': form}, template_name="blog/post_edit.html")
+        
+    def update(self, request, pk):
+        if request.method == 'POST':
+            post = get_object_or_404(Post, pk=pk)
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            post = get_object_or_404(Post, pk=pk)
+            if resolve(request.path_info).url_name == 'post_publish':
+                post.publish()
+                return redirect('post_detail', pk=pk)
+            form = PostForm(instance=post)
+        return Response({'form': form}, template_name="blog/post_edit.html")
+    
+    def destroy(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        post.delete()
+        return redirect('post_list')
+
+class CommentViewSet(viewsets.ModelViewSet):
+
+    renderer_classes = [TemplateHTMLRenderer]
+
+    def create(self, request, pk):
+        if request.method == 'POST':
+            post = get_object_or_404(Post, pk=pk)
+            form = CommentForm(request.POST)
+            if form.is_valid():
+                comment = form.save(commit=False)
+                comment.post = post
+                comment.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            post = get_object_or_404(Post, pk=pk)
+            form = CommentForm()
+
+        return Response({'form': form}, template_name="blog/add_comment_to_post.html")
+    
+    def update(self, request, pk):
+        comment = get_object_or_404(Comment, pk=pk)
+        comment.approve()
+        return redirect('post_detail', pk=comment.post.pk)
+    
+    def destroy(self, request, pk):
+        comment = get_object_or_404(Comment, pk=pk)
+        comment.delete()
+        return redirect('post_detail', pk=comment.post.pk)
+
+"""
 class PostList(APIView):
 
     renderer_classes = [TemplateHTMLRenderer]
@@ -17,8 +97,8 @@ class PostList(APIView):
     def get(self, request):
         posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
         return Response({'posts': posts})
-
-
+"""
+"""
 class PostDetail(APIView):
 
     renderer_classes = [TemplateHTMLRenderer]
@@ -32,9 +112,10 @@ class PostDetail(APIView):
 
     def get(self, request, pk):
         post = self.get_object(pk)
-        return Response({'post': post})
-    
-
+        return Res D<   ponse({'post': post})
+        return Res Dponse({'post': post})
+"""
+"""
 class PostNew(APIView):
 
     renderer_classes = [TemplateHTMLRenderer]
@@ -48,11 +129,11 @@ class PostNew(APIView):
             post.save()
             return redirect('post_detail', pk=post.pk)
         
-    def get(self,request):
+    def get(self, request):
         form = PostForm()
         return Response({'form': form})
-    
-
+"""    
+"""
 class PostEdit(APIView):
 
     renderer_classes = [TemplateHTMLRenderer]
@@ -77,7 +158,8 @@ class PostEdit(APIView):
             post.author = request.user
             post.save()
             return redirect('post_detail', pk=post.pk)
-        
+"""
+"""
 class PostDraftList(APIView):
 
     renderer_classes = [TemplateHTMLRenderer]
@@ -86,23 +168,24 @@ class PostDraftList(APIView):
     def get(self, request):
         posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
         return Response({'posts': posts})
-    
-
+"""    
+"""
 class PostPublish(APIView):
 
     def get(self, request, pk):
         post = get_object_or_404(Post, pk=pk)
         post.publish()
         return redirect('post_detail', pk=pk)
-    
-
+"""
+"""
 class PostRemove(APIView):
     
     def get(self, request, pk):
         post = get_object_or_404(Post, pk=pk)
         post.delete()
         return redirect('post_list')
-    
+"""    
+"""
 class AddCommentToPost(APIView):
 
     renderer_classes = [TemplateHTMLRenderer]
@@ -127,18 +210,20 @@ class AddCommentToPost(APIView):
             comment.post = post
             comment.save()
             return redirect('post_detail', pk=post.pk)
-        
-
+"""
+"""
 class CommentApprove(APIView):
         
     def get(self, request, pk):
         comment = get_object_or_404(Comment, pk=pk)
         comment.approve()
         return redirect('post_detail', pk=comment.post.pk)
-    
+"""
+"""
 class CommentRemove(APIView):
 
     def get(self, request, pk):
         comment = get_object_or_404(Comment, pk=pk)
         comment.delete()
         return redirect('post_detail', pk=comment.post.pk)
+"""
